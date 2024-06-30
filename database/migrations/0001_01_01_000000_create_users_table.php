2669cfff228abe76debd286925461055fbefe78a
MRHRTZ <hanifsyauqi61@gmail.com>
2024-06-30 15:29:38 +0700

    prepare db migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('room_id')->nullable();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['ADMIN', 'PEMBIMBING', 'PESERTA'])->default('PESERTA');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2024_06_29_161858_create_attendance_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendance', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->constrained('users')->cascadeOnDelete();
+            $table->unsignedBigInteger('room_id')->nullable()->constrained('rooms')->cascadeOnDelete();
+            $table->string('nim', 15);
+            $table->string('location');
+            $table->string('coordinate');
+            $table->text('activity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendance');
+    }
+};
